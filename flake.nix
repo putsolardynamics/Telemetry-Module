005b408cff59ed5a29ce8ca814799e9b9908e29f
kamo104 <kamo.gabo@gmail.com>
2024-04-23 13:48:03 +0200

    added flake for stm32pio

--- flake.nix
+++ flake.nix
@@ -7,20 +7,36 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true;};
       });
     in
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
+        pkgs.mkShell =  {
           packages = with pkgs; [
-            stm32cubemx
-            
-            python311Full
-            python311Packages.pip
+            # stm32cubemx
+            (pkgs.callPackage /home/kamo/Projects/Telemetry-Module-copy/stm32cubemx.nix {})
+
+            # (pkgs.callPackage ./stm32cubemx.nix {})
+            # python311Full
+            # python311Packages.pip
+            (python311.withPackages (ps: with ps; [
+                pip
+                virtualenv
+                # (ps.callPackage ./stm32pio.nix {})
+            ]))
+            # virtualenv
             platformio
+            jdk17
+
+            clang-tools
+            gnumake
           ];
         };
+        shellHook = ''
+          echo hello world
+          TEST=ajdsijiadsij
+        '';
       });
     };
 }
