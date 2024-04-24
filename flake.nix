59d09df28081de940ddee2f5a8cd16fb39889d98
kamo104 <kamo.gabo@gmail.com>
2024-04-24 12:04:15 +0200

    working flake environment

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "A Nix-flake-based Nix development environment";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  # inputs.stm32cubemx.url = "path:./stm32cubemx";
 
   outputs = { self, nixpkgs }:
     let
@@ -12,31 +13,35 @@
     in
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
-        pkgs.mkShell =  {
+        default = pkgs.mkShell {
           packages = with pkgs; [
             # stm32cubemx
-            (pkgs.callPackage /home/kamo/Projects/Telemetry-Module-copy/stm32cubemx.nix {})
-
-            # (pkgs.callPackage ./stm32cubemx.nix {})
-            # python311Full
-            # python311Packages.pip
+            (pkgs.callPackage /home/kamo/Projects/Telemetry-Module/stm32cubemx.nix {}) # nixos has an old version of cubemx
             (python311.withPackages (ps: with ps; [
                 pip
-                virtualenv
-                # (ps.callPackage ./stm32pio.nix {})
             ]))
-            # virtualenv
             platformio
             jdk17
+            # stlink #stlink needs to have udev rules installed (in nixos it's services.udev... [pkgs.stlink])
+            picocom
 
             clang-tools
             gnumake
           ];
+          shellHook = ''
+            # ./setup.sh            
+
+            if [ ! -d ".env" ]; then
+              echo "installing..."
+              python3 -m venv .env
+              source ".env/bin/activate"
+              pip install stm32pio
+              echo "done"
+            fi
+
+            source .env/bin/activate
+          '';   
         };
-        shellHook = ''
-          echo hello world
-          TEST=ajdsijiadsij
-        '';
       });
     };
 }
